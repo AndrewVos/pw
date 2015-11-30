5ff0178049cbdd26d95ea2524a83d0b425cdf2e2
Andrew Vos <andrew.vos@gmail.com>
2015-11-30 11:43:06 +0000

    Don't crash when no passwords file exists

--- pw_autocomplete.sh
+++ pw_autocomplete.sh
@@ -6,6 +6,10 @@ PASSWORDS_FILE=$PW_PATH/passwords.gpg
 
 if [ "$SHELL" = "/bin/bash" ]; then
   function _complete_pw() {
+    if [ ! -f $PASSWORDS_FILE ]; then
+      return 0
+    fi
+
     local word=${COMP_WORDS[COMP_CWORD]}
 
     lines="$(gpg2 --decrypt --quiet $PASSWORDS_FILE | grep "s: ")"
